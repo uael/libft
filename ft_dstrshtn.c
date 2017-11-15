293538f1f58266e6876e7995f1ca547e2d183b4f
uael <lucas.abel@yanedx.com>
2017-11-15 16:15:35 +0100

    gnl fixes

--- ft_dstrshtn.c
+++ ft_dstrshtn.c
@@ -16,13 +16,19 @@ inline t_usz	ft_dstrshtn(t_dstr *self, t_usz n, t_car *out)
 {
 	t_car *it;
 
-	if (self->len == 0)
+	if (!n || self->len == 0)
 		return (0);
 	if (n > self->len)
 		n = self->len;
 	it = ft_dstrbeg(self);
 	if (out)
 		ft_memcpy(out, it, n * sizeof(t_car));
-	ft_memmove(it, it + n, (self->len - n - 1) * sizeof(t_car));
+	if (n == self->len)
+		self->len = 0;
+	else
+	{
+		ft_memmove(it, it + n, (self->len -= n) * sizeof(t_car));
+		it[self->len] = '\0';
+	}
 	return (n);
 }
